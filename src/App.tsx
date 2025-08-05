180bfd3781ec886632b7f2239afc5a9c25a99d89
solarand <den.23-02@mail.ru>
2025-08-05 19:32:06 +0300

    сверстана старница логина/регистрации

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 // import StartPage from "./pages/StartPage/StartPage";
 import StartPage from "@/pages/StartPage/StartPage";
+import LoginPage from "./pages/auth/loginPage";
+import RegisterPage from "./pages/auth/registerPage";
 
 function App() {
   return (
     <>
-      <StartPage />
+      {/*<StartPage />*/}
+      <RegisterPage />
     </>
   );
 }
